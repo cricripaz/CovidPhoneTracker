61498c3f2b2ec049dc8d6856ec20e32763a992c5
SamuelEscobar761 <samueb8@gmail.com>
2021-09-08 19:47:45 -0400

    AddNumbersActivity

---
+++ app/src/main/java/com/example/covidphonetracker/UnionFind.kt
@@ -0,0 +1,30 @@
+package com.example.covidphonetracker
+
+class UnionFind (n : Int ) {
+    private val roots = IntArray(n){idx -> idx}
+    private val ranks = IntArray(n){ 1 }
+
+    fun find(i: Int): Int{
+        if(roots[i] != i){
+            roots[i] = find(roots[i])
+        }
+
+        return roots[i]
+    }
+
+
+    fun union(x: Int, y: Int) {
+        val rootX = find(x)
+        val rootY = find(y)
+
+        if (ranks[rootX] > ranks[rootY]) {
+            roots[rootY] = rootX
+            ++ranks[rootX]
+        } else {
+            roots[rootX] = rootY
+            ++ranks[rootY]
+        }
+    }
+
+
+}
