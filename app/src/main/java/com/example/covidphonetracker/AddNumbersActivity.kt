c4b035061962f30e125ad229bb542d2c631fe68c
cricripaz <49013118+cricripaz@users.noreply.github.com>
2021-09-08 22:14:20 -0400

    Revert "init proyecto"
    This reverts commit 42348ddb83e7a5c5a1125ee6bf876e71856512fa.

---
+++ app/src/main/java/com/example/covidphonetracker/AddNumbersActivity.kt
@@ -0,0 +1,24 @@
+package com.example.covidphonetracker
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AddNumbersActivity : AppCompatActivity() {
+
+    private lateinit var binding: AddNumbersActivity
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_add_numbers)
+
+
+
+
+
+    }
+}
+
+
