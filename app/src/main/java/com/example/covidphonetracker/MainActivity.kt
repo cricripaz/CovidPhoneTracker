2068084c86a3c46bfcaefbc7820079d26082d1db
cricripaz <49013118+cricripaz@users.noreply.github.com>
2021-09-17 20:45:40 -0400

    Mejorando Disenio de la aplicacion

--- app/src/main/java/com/example/covidphonetracker/MainActivity.kt
+++ app/src/main/java/com/example/covidphonetracker/MainActivity.kt
@@ -31,12 +31,6 @@ class MainActivity : AppCompatActivity() {
         }
 
 
-
-        binding.card01.setOnClickListener {
-            val intent = Intent(this , ListNumbersActivty::class.java)
-            startActivity(intent)
-        }
-
         binding.card10.setOnClickListener{
             val intent = Intent(this, NotificationsActivity::class.java)
             startActivity(intent)
